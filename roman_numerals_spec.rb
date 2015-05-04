1d41e6adc78473e563d52042f51ff7696e748aae
JR <jroger2908@gmail.com>
2015-05-04 11:00:32 -0400

    Second rspec test written for converting 3 to III

--- roman_numerals_spec.rb
+++ roman_numerals_spec.rb
@@ -1,10 +1,14 @@
 require_relative 'roman_numerals'
 
 describe "RomanNumeral" do 
-  
+
   describe "convert" do
     it "converts 1 to I" do
       expect(RomanNumeral.convert(1)).to eq("I")
+    end
+
+    it "converts 3 to III" do
+      expect(RomanNumeral.convert(3)).to eq("III")
     end
   end
 
