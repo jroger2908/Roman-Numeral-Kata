0c1af5a3d2f1f4a5e208e4afbfd5ada7dd29f822
JR <jroger2908@gmail.com>
2015-05-04 16:54:01 -0400

    Added case statements for 4s and 9s like IX and IV. Added a length variable because IX needs to remove two characters from string. This length is used as the second argument in slice! All tests are passing

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -35,16 +35,31 @@ class ArabicNumeral
     arabic_number = 0
     until roman_numeral.empty?
       case 
-      when roman_numeral.start_with?('M') then value = 1000
+      when roman_numeral.start_with?('M') then value = 1000 
+        length = 1
+      when roman_numeral.start_with?('CM') then value = 900
+        length = 2
       when roman_numeral.start_with?('D') then value = 500
+        length = 1
+      when roman_numeral.start_with?('CD') then value = 400
+        length = 2
       when roman_numeral.start_with?('C') then value = 100
+        length = 1
       when roman_numeral.start_with?('L') then value = 50
+        length = 1
       when roman_numeral.start_with?('X') then value = 10
+        length = 1
+      when roman_numeral.start_with?('IX') then value = 9
+        length = 2
       when roman_numeral.start_with?('V') then value = 5
+        length = 1
+      when roman_numeral.start_with?('IV') then value = 4
+        length = 2
       when roman_numeral.start_with?('I') then value = 1
+        length = 1
       end
       arabic_number += value
-      roman_numeral.slice!(0, 1)
+      roman_numeral.slice!(0, length)
     end
     arabic_number
   end
